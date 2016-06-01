85c61bd5da3e7d1616054ece8ccec811b2d3a002
Igor Chepurnoy <igorzfort@gmail.com>
2016-06-01 15:58:04 +0300

    Update ScheduleController.php

--- ScheduleController.php
+++ ScheduleController.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace omnilight\scheduling;
+namespace yii2mod\scheduling;
+
+use Yii;
 use yii\console\Controller;
 use yii\di\Instance;
 
-
 /**
- * Run the scheduled commands
+ * Class ScheduleController
+ * @package yii2mod\scheduling
  */
 class ScheduleController extends Controller
 {
@@ -14,11 +16,17 @@ class ScheduleController extends Controller
      * @var Schedule
      */
     public $schedule = 'schedule';
+
     /**
      * @var string Schedule file that will be used to run schedule
      */
     public $scheduleFile;
 
+    /**
+     * Returns the names of valid options for the action (id)
+     * @param string $actionID
+     * @return array
+     */
     public function options($actionID)
     {
         return array_merge(parent::options($actionID),
@@ -26,49 +34,58 @@ class ScheduleController extends Controller
         );
     }
 
-
+    /**
+     * Initializes the object.
+     * @throws \yii\base\InvalidConfigException
+     */
     public function init()
     {
-        if (\Yii::$app->has($this->schedule)) {
+        if (Yii::$app->has($this->schedule)) {
             $this->schedule = Instance::ensure($this->schedule, Schedule::className());
         } else {
-            $this->schedule = \Yii::createObject(Schedule::className());
+            $this->schedule = Yii::createObject(Schedule::className());
         }
         parent::init();
     }
 
-
+    /**
+     * Run scheduled commands
+     * @return void
+     */
     public function actionRun()
     {
         $this->importScheduleFile();
 
-        $events = $this->schedule->dueEvents(\Yii::$app);
+        $events = $this->schedule->dueEvents(Yii::$app);
 
         foreach ($events as $event) {
-            $this->stdout('Running scheduled command: '.$event->getSummaryForDisplay()."\n");
-            $event->run(\Yii::$app);
+            $this->stdout('Running scheduled command: ' . $event->getSummaryForDisplay() . "\n");
+            $event->run(Yii::$app);
         }
 
-        if (count($events) === 0)
-        {
+        if (count($events) === 0) {
             $this->stdout("No scheduled commands are ready to run.\n");
         }
     }
 
+    /**
+     * Import schedule file
+     * @return void
+     */
     protected function importScheduleFile()
     {
         if ($this->scheduleFile === null) {
             return;
         }
 
-        $scheduleFile = \Yii::getAlias($this->scheduleFile);
+        $scheduleFile = Yii::getAlias($this->scheduleFile);
         if (file_exists($scheduleFile) == false) {
-            $this->stderr('Can not load schedule file '.$this->scheduleFile."\n");
+            $this->stderr('Can not load schedule file ' . $this->scheduleFile . "\n");
             return;
         }
 
         $schedule = $this->schedule;
-        call_user_func(function() use ($schedule, $scheduleFile) {
+        call_user_func(function () use ($schedule, $scheduleFile) {
             include $scheduleFile;
         });
     }
